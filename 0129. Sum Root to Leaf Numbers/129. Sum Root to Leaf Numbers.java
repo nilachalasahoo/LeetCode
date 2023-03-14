6635248dd986e81681473ed8248183e118427bbd
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-03-14 16:26:00 +0530

    Create 129. Sum Root to Leaf Numbers.java

---
+++ 0129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sum(TreeNode root,int sum){
+        if(root==null)
+            return 0;
+        if(root.left==null && root.right==null)
+            return sum*10+root.val;
+        int left=sum(root.left,sum*10+root.val);
+        int right=sum(root.right,sum*10+root.val);
+        return left+right;
+    }
+    public int sumNumbers(TreeNode root) {
+        return sum(root,0);
+    }
+}
