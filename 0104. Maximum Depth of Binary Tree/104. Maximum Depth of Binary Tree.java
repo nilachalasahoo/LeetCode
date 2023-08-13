694e9bef0a6b07b9f96f9473e83e6d11943f5f5a
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-13 13:44:27 +0530

    Create 104. Maximum Depth of Binary Tree.java

---
+++ 0104. Maximum Depth of Binary Tree/104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        int c=0;
+        int x=maxDepth(root.left);
+        int y=maxDepth(root.right);
+        if(x>y)
+            c=x;
+        else
+            c=y;
+        return c+1;
+    }
+}
