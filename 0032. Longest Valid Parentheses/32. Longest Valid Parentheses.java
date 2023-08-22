3c11be75f14c232a16a5199fb82a47852c7a7bf3
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2023-08-22 23:46:50 +0530

    Create 32. Longest Valid Parentheses.java

---
+++ 0032. Longest Valid Parentheses/32. Longest Valid Parentheses.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st=new Stack<>();
+        int max=0;
+        st.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+                st.push(i);
+            else{
+                st.pop();
+                if(!st.isEmpty())
+                    max=Math.max(max,i-st.peek());
+                else
+                    st.push(i);
+            }
+        }
+        return max;
+    }
+}
